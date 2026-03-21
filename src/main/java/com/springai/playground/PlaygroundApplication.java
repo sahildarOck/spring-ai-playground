615df8fe666151bb1b22fa1bfc20504411230a57
Sahil Srivastava <darock@Sahils-MacBook-Air.local>
2026-03-21 16:52:37 +0530

    chore: updated package name

--- src/main/java/com/springai/playground/PlaygroundApplication.java
+++ src/main/java/com/springai/playground/PlaygroundApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.springai.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PlaygroundApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PlaygroundApplication.class, args);
 	}
 
 }
--- src/test/java/com/springai/playground/PlaygroundApplicationTests.java
+++ src/test/java/com/springai/playground/PlaygroundApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.springai.playground;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PlaygroundApplicationTests {
 
 	@Test
 	void contextLoads() {
